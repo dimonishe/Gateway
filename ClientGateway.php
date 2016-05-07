c1c32b48d9b03aa7ff8bc48f982372c14638f29c
dem0nSH <dem0n12@mail.ru>
2016-05-07 23:44:32 +0200

    07.05.2016 Deleted index.php and namespace. Rewrited ClientGateway class and added get time method.

--- ClientGateway.php
+++ ClientGateway.php
@@ -1,15 +1,14 @@
 <?php
-
-namespace ClientGateway;
-
-use Exception;
-
 class ClientGateway{
     
     private $socket = null;
     private $IP = null;
     private $Port = null;
-
+    /**
+     * Initialize all necessary variables and involve methods realized bellow
+     * @param string $IP
+     * @param string $Port
+     */
     public function __construct($IP, $Port){
         
         $this->IP = $IP;
@@ -17,39 +16,45 @@ class ClientGateway{
         
         try{
             $this->createSocket();
+            $this->openGate();  
         } catch (Exception $ex) {
             echo $ex->getMessage();
         }
     }
-    
+    /**
+     * Function create a socket for connection to server
+     * @throws Exception
+     */
     private function createSocket(){
-
-        $this->socket = \fsockopen($this->IP, $this->Port, $errno, $errstr, 30);
+        $this->socket = fsockopen($this->IP, $this->Port, $errno, $errstr, 30);
         if (!$this->socket) {
             throw new Exception("$errstr ($errno)<br />\n");
         }
     }
-    
-    private function sendMessage(){
-        
-        $out = md5("Open");
-        
+    /**
+     * Function open gate with using md5 hash
+     * @throws Exception
+     */
+    private function openGate(){
+        $out = md5($this->getTime());
         if(!fwrite($this->socket, $out)){
             throw new Exception("Connection error!");
         }
-        
         echo "Opening door...";
-        
         fclose($this->socket);
-        
     }
-
-    public function openGate() {
-        try {
-            $this->sendMessage();
-        } catch (Exception $ex) {
-            echo $ex->getMessage();
-        }
+    /**
+     * Get time from api of api.geonames.org and take time from received data by regexp
+     * @return string
+     */
+    private function getTime(){
+        $file = file_get_contents("http://api.geonames.org/timezone?lat=47.01&lng=10.2&username=demo&style=full");
+        preg_match("/(....-..-..\s..:..)/", $file, $result);
+        return $result[0];
     }
-
 }
+
+static $IP = "10.42.0.22";
+static $PORT = "4444";
+
+$client = new ClientGateway($IP, $PORT);
